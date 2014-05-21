822fb99f639362a19d37038cca8926fb28114e21
richard-gardiner <richard_gardiner@bluebottle.com>
2014-05-21 21:31:11 +0100

    Groupbox white background issue resolved

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,7 +17,11 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
 	// Group the sets of cards to make
 	// it easier to move them together
 	PlayersCards = new QGroupBox(this);
-	DealersCards = new QGroupBox(this);
+	DealersCards = new QGroupBox(this)
+	
+	// Corrected schoolboy error - RG21-05-2014
+	PlayersCards->setFlat(true);
+	DealersCards->setFlat(true);
 
 	// Set the size and geometry of each card hand container
 	PlayersCards->setGeometry(195*XScale, 310*YScale, 300*XScale, 300*YScale);
